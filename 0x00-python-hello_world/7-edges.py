6e9fedfbe940e228a110ab801281c8adfa4139c4
hmachacon <hmachacom@gmail.com>
2021-12-20 20:40:09 +0000

    add variables

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[-2:]))
-middle_word = word[-8:-1]
-#middle_word = middle_word[-7:]
+word_last_2 = word[-2:]
+word_first_3= word[:3]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+middle_word = word[:-1]
+middle_word = middle_word[-7:]
 print("Middle word: {}".format(middle_word))
