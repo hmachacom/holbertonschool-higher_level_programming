6bfb358c9c5f9f1043ac377aa7c3e2956428c23b
hmachacon <hmachacom@gmail.com>
2021-12-20 20:23:30 +0000

    add file 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+middle_word = word[:-1]
+middle_word = middle_word[-7:]
+print("Middle word: {}".format(middle_word))
