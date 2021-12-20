91c3034da99eebf04e0d362a6c892bb04461946c
hmachacon <hmachacom@gmail.com>
2021-12-20 21:05:38 +0000

    add var

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3= word[0:3]
+word_first_3 = word[0:3]
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
 print("Last 2 letters: {}".format(word_last_2))
