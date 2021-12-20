d888c9209f917520ac333d2fa6ed880ae302c108
hmachacon <hmachacom@gmail.com>
2021-12-20 20:30:39 +0000

    add file 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[-2:]))
-middle_word = word[:-1]
-middle_word = middle_word[-7:]
+middle_word = word[-8:-1]
+#middle_word = middle_word[-7:]
 print("Middle word: {}".format(middle_word))
