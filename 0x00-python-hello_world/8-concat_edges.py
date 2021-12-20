615148b6044194ab32f1a60f4d6ad31a92550cf2
hmachacon <hmachacom@gmail.com>
2021-12-20 21:29:37 +0000

    add files 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:66] + str[106:-17] + str[0:-123])
+print(str[39:66] + str[106:-17] + str[:6])
