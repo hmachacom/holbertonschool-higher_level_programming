06d3b8fee8c8f2cf38eda95d2f02e64d274283cc
hmachacon <hmachacom@gmail.com>
2021-12-21 22:25:29 +0000

    coreccion imprimir por ciclo

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    str2 = ""
     for charter in str:
         charter2 = ord(charter)
         if charter2 >= 97 and charter2 <= 122:
-            str2 += chr(ord(charter) - 32)
+            charter2 = chr(ord(charter) - 32)
         else:
-            str2 += charter
-    print("{:s}".format(str2))
+            charter2 = charter
+        print("{:s}".format(charter2), end="")
+    print("")
