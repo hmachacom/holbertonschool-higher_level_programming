014beb648f3d97dc01492ad9c938337a79defa37
hmachacon <hmachacom@gmail.com>
2021-12-21 22:14:14 +0000

    coreccion de formato

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     str2 = ""
     for charter in str:
         charter2 = ord(charter)
-        if charter2 in range(97, 123):
+        if charter2 >= 97 and charter2 <= 122:
             str2 += chr(ord(charter) - 32)
         else:
             str2 += charter
-    print("{}".format(str2))
+    print("{:s}".format(str2))
