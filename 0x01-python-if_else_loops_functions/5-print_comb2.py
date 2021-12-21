adac26f7e190be2f55d827e2474bf9737d9294cb
hmachacon <hmachacom@gmail.com>
2021-12-21 18:43:51 +0000

    correct style

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for abc in range(0, 100):
-	if abc < 10:
-		aux = "0"
-	if abc < 99 :
-		print("{}{:d}".format(aux, abc), end=", ")
-	else :
-		print("{:d}".format(abc))
-	aux = ""
+    if abc < 10:
+        aux = "0"
+    if abc < 99:
+        print("{}{:d}".format(aux, abc), end=", ")
+    else:
+        print("{:d}".format(abc))
+    aux = ""
