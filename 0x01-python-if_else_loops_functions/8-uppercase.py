70cee27091a5ce1c4acf57d8987d19a895e85aef
hmachacon <hmachacom@gmail.com>
2021-12-21 22:00:30 +0000

    add function uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    str2 = ""
+    for charter in str:
+        charter2 = ord(charter)
+        if charter2 in range(97, 123):
+            str2 += chr(ord(charter) - 32)
+        else:
+            str2 += charter
+    print(str2)
