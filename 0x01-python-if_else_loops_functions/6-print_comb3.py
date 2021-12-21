16b91ce2d3968bb1dba60f34f02f0cc17b61db11
hmachacon <hmachacom@gmail.com>
2021-12-21 20:31:29 +0000

    correccion

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for abc in range(0, 100):
-    if abc < 10:
-        aux = "0"
-    if abc < 99:
-        print("{}{:d}".format(aux, abc), end=", ")
-    else:
-        print("{:d}".format(abc))
-    aux = ""
+for num1 in range(0, 9):
+    for num2 in range(1, 10):
+        if num1 < 8:
+            print("{:d}{:d}".format(num1, num2), end=", ")
+        elif num2 == 9:
+            print("{:d}{:d}".format(num1, num2))
