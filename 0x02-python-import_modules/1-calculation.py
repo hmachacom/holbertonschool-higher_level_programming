3c86337a1e1b4b2cf8583044e29d25d12deac727
hmachacon <hmachacom@gmail.com>
2021-12-26 17:05:13 +0000

    add 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import calculator_1 as calc
-    a = 10
-    b = 5
+import calculator_1 as calc
+a = 10
+b = 5
 print(f"{a} + {b} = {calc.add(a, b)}")
 print(f"{a} - {b} = {calc.sub(a, b)}")
 print(f"{a} * {b} = {calc.mul(a, b)}")
