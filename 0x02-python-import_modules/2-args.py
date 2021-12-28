b4ceba3a1586035e0353f47a0b22f1f240cc643f
hmachacon <hmachacom@gmail.com>
2021-12-28 21:40:27 +0000

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,7 @@ if __name__ == "__main__":
     long = len(argv)
     if long == 1:
         print("0 arguments.")
-    elif long == 2:
-        print("{} arguments:".format(long - 1))
-        print("1: {}".format(argv[1]))
-    else:
+    elif long > 1:
         print("{} arguments:".format(long - 1))
         for argc in range(1, long):
             print("{}: {}".format(argc, argv[argc]))
