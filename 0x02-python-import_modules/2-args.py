68a337ad4cdc30a755760c0db90751bde1593b6b
hmachacon <hmachacom@gmail.com>
2021-12-28 20:48:38 +0000

    add file 2-argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     long = len(sys.argv)
     if long == 1:
         print("0 arguments.")
-    elif long > 1:
+    elif long == 2:
+        print("{} arguments:".format(long - 1))
+        print("1: {}".format(sys.argv[1]))
+    else:
         print("{} arguments:".format(long - 1))
         for argc in range(1, long):
             print("{}: {}".format(argc, sys.argv[argc]))
