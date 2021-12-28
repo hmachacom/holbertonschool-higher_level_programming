9408eb1c094edc3155afbcb026e599d2313bb6d9
hmachacon <hmachacom@gmail.com>
2021-12-28 13:39:35 +0000

    ad

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from add_0 import add
+    from add_0 import add as sum
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} + {b} = {sum(a, b)}")
