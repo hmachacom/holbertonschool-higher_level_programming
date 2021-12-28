4255d746da24eb580b42746b511c24b15dc9c784
hmachacon <hmachacom@gmail.com>
2021-12-28 13:46:30 +0000

    file correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 import calculator_1 as calc
 a = 10
 b = 5
-print(f"{a} + {b} = {calc.add(a, b)}")
-print(f"{a} - {b} = {calc.sub(a, b)}")
-print(f"{a} * {b} = {calc.mul(a, b)}")
-print(f"{a} / {b} = {calc.div(a, b)}")
+print("{} + {} = {}".format(a, b, calc.add(a, b)))
+print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+print("{} / {} = {}".format(a, b, calc.div(a, b)))
