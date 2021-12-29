16a99ca461028c1217ad3ecc1484f060445ee24f
hmachacon <hmachacom@gmail.com>
2021-12-29 22:04:04 +0000

    dictionari

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,20 +5,17 @@ import calculator_1 as calc
 
 if __name__ == "__main__":
     long = len(sys.argv)
-    operator = ["+", "/", "*", "-"]
+    operator = {"+": calc.add, "/": calc.div, "*": calc.mul, "-": calc.sub}
     argc = sys.argv
-    if long != 4 and argc[2] != "*":
+    if long != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif not(sys.argv[2] in operator):
         print("Unknown operator. Available operators: +, -, * and / ")
         exit(1)
     else:
-        clac = ([calc.add, calc.div, calc.mul, calc.sub])
-        selct = 0
-        for op in operator:
-            if op == sys.argv[2]:
-                result = clac[selct](int(sys.argv[1]), int(sys.argv[3]))
-                print("{} + {} = {}".format(sys.argv[1], sys.argv[3], result))
-                exit(0)
-            selct += 1
+        result = operator[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
+        a = sys.argv[1]
+        op = sys.argv[2]
+        print("{} {} {} = {}".format(a, op, sys.argv[3], result))
+        exit(0)
