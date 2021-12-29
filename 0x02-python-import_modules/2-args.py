376815074bae641030b078b5e1095c152e78b417
hmachacon <hmachacom@gmail.com>
2021-12-29 16:51:14 +0000

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     if long == 1:
         print("0 arguments.")
     elif long > 1:
-        print("{} arguments:".format(long - 1))
+        if long == 2:
+            print("{} argument:".format(long - 1))
+        else:
+            print("{} arguments:".format(long - 1))
         for argc in range(1, long):
             print("{}: {}".format(argc, argv[argc]))
