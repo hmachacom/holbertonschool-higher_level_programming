010a640a24090607f2fe21f294ebb866e9b3d761
hmachacon <hmachacom@gmail.com>
2022-01-03 21:11:19 +0000

    add file 11-delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     long = len(my_list)
     if long == 0:
-        return None
+        return my_list
     elif idx > long - 1 or idx < 0:
         return my_list
     del my_list[idx]
