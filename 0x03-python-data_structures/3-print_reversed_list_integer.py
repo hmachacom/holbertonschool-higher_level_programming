cc69c6d3cfe19e24e7fcea0f442e1ec2aa5ffdfc
hmachacon <hmachacom@gmail.com>
2022-01-03 19:14:25 +0000

    add conditions

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     count = len(my_list)
     for i in range(count, 0, -1):
-        print("{}".format(my_list[i - 1]))
+        print("{:d}".format(my_list[i - 1]))
