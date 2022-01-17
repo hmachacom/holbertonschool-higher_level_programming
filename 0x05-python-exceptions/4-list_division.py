208638675c93217e53f40298f9f1774307c0886a
hmachacon <hmachacom@gmail.com>
2022-01-17 17:37:15 +0000

    err

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,20 @@
 #!/user/bin/python3
-from hashlib import new
-from operator import ne
-
-
 def list_division(my_list_1, my_list_2, list_length):
-	new_list = []
-	e = 0
-	num = 0
-	for i in range(list_length):
-		try:
-			num = my_list_1[i] / my_list_2[i]
-			new_list.append(num)
-		except ZeroDivisionError:
-			num = 0
-		except IndexError:
-			num = 0
-		except ValueError:
-			num = 0
-		except TypeError:
-			num = 0
-		finally:
-			new_list.append(num)
-	return new_list
+    new_list = []
+    e = 0
+    num = 0
+    for i in range(list_length):
+        try:
+            num = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            num = 0
+        except IndexError:
+            print("out of range")
+            num = 0
+        except TypeError:
+            print("wrong type")
+            num = 0
+        finally:
+            new_list.append(num)
+    return new_list
