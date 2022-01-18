720601b58ab1d527d3f06ca6cf82a5355a9b1a8e
Hugo Machacon <hmachacom@gmail.com>
2022-01-18 17:26:47 -0500

    add file 5-square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" class square new """
+
+
+class Square:
+    """class square empty"""
+
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        """ Validations errors """
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """return area of the square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print a square"""
+        areas = self.__size
+        for i in range(areas):
+            for j in range(areas):
+                print("#", end="")
+            print()
