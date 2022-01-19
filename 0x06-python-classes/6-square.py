3fc417bea7936d9c286267c36ba76a02a5e524b6
Hugo Machacon <hmachacom@gmail.com>
2022-01-18 21:22:30 -0500

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     pass
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
