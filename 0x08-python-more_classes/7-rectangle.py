0ee15a5d4ad79864a9da6e4841655d919893dc5e
Hugo Machacon <hmachacom@gmail.com>
2022-01-24 15:29:09 -0500

    add file 7-rectangle

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """
+    Class Rectangle
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+        self.print_symbol = "#"
+
+    @property
+    def height(self):
+        """Value height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Validations errors"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """ Value width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Validations errors"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """Rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Perimeter rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """print charter #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(
+            str(self.print_symbol) * self.__width for i in range(self.__height)
+        )
+
+    def __repr__(self):
+        """ Print instans for developmen"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Delete Rectangle"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
