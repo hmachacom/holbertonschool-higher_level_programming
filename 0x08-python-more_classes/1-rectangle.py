f4c60f79d2ec327f47ede4ab22a9315571c2cdf2
Hugo Machacon <hmachacom@gmail.com>
2022-01-24 18:15:33 -0500

    noswe

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,6 +15,20 @@ class Rectangle:
         self.height = height
 
     @property
+    def width(self):
+        """ Value width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Validations errors"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
     def height(self):
         """Value height"""
         return self.__height
@@ -28,16 +42,3 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__height = value
 
-    @property
-    def width(self):
-        """ Value width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Validations errors"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
