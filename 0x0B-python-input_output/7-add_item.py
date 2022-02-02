644152a8d09346de9b232f55b9daed40484e0194
Hugo Machacon <hmachacom@gmail.com>
2022-02-01 20:43:26 -0500

    add try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,12 +10,10 @@ read_file = __import__("0-read_file").read_file
 filename = "add_item.json"
 li = []
 
-with open(filename, mode="a", encoding="utf-8"):
-    with open(filename, mode="r", encoding="utf-8") as f:
-        if len(f.read()) != 0:
-            li = load_from_json_file(filename)
-            for i in sys.argv[1:]:
-                li.append(i)
-            save_to_json_file(li, filename)
-        else:
-            save_to_json_file(li, filename)
+try:
+	li = load_from_json_file(filename)
+except Exception:
+    li = []
+for i in sys.argv[1:]:
+	li.append(i)
+save_to_json_file(li, filename)
