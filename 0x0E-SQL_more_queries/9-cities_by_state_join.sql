5b16d28a3c09dc1572b5e306002a4d1f94a5e954
Hugo Machacon <hmachacom@gmail.com>
2022-03-16 20:46:33 -0500

    add inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states ON hbtn_0d_usa.states.id = hbtn_0d_usa.cities.state_id ORDER BY  cities.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY  cities.id;
