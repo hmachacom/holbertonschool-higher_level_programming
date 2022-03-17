b7e602143de84a72c7b3f60369a23eb9f7ea1c34
Hugo Machacon <hmachacom@gmail.com>
2022-03-17 06:25:50 -0500

    soy el puto amo jajaaja

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Genre ID by show
-SELECT tv_genres.name,COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres 
+SELECT tv_genres.name AS genre,COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres 
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
