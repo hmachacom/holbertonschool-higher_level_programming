1e29cae812e67085f95e71c43545f0324f698734
Hugo Machacon <hmachacom@gmail.com>
2022-04-04 13:08:34 -0500

    JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,10 @@
 const { argv } = require('process');
 
 // print process.argv
-if (argv.length === 2) {
+if (argv.length === 3) {
   console.log('Argument found');
 } else {
-  if (argv.length > 2) {
+  if (argv.length > 3) {
     console.log('Arguments found');
   } else {
     console.log('No argument');
