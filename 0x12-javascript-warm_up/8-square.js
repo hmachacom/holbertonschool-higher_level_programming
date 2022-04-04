2532977f66311c8ff4b8518a1b6aec3428536a7e
Hugo Machacon <hmachacom@gmail.com>
2022-04-04 14:49:49 -0500

    8-file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+// print process.argv
+if (argv[2] && !(isNaN(parseInt(argv[2])))) {
+	let num = parseInt(argv[2]);
+	for (; num; num--) {
+    console.log('X'.repeat(parseInt(argv[2])));
+  }
+} else {
+  console.log('Missing size');
+}
