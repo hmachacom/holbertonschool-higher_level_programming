16b584e0d4fffefbd01ee0b8d971c3ca5e2a4647
Hugo Machacon <hmachacom@gmail.com>
2022-04-04 14:16:15 -0500

    7-file

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+// print process.argv
+if (argv[2] && !(isNaN(parseInt(argv[2])))) {
+  const x = parseInt(argv[2]);
+  for (let index = 0; index <= x; index++) {
+    console.log('C is  fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
