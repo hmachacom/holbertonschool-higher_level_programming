61eaa0e171458e0851b1bbe5e6696c9abcea7d2e
Hugo Machacon <hmachacom@gmail.com>
2022-04-04 14:50:31 -0500

    8-file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const { argv } = require('process');
 
 // print process.argv
 if (argv[2] && !(isNaN(parseInt(argv[2])))) {
-	let num = parseInt(argv[2]);
-	for (; num; num--) {
+  let num = parseInt(argv[2]);
+  for (; num; num--) {
     console.log('X'.repeat(parseInt(argv[2])));
   }
 } else {
