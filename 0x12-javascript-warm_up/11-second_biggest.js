f1065134fb895ea6f50ecd2101aa378b41925cd2
Hugo Machacon <hmachacom@gmail.com>
2022-04-04 15:52:54 -0500

    9-file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+// print process.argv
+if (!argv[2] || argv.length === 3) {
+  console.log(0);
+} else {
+  const newAr = argv.slice(2).sort();
+  newAr.pop();
+  console.log(newAr[newAr.length - 1]);
+}
