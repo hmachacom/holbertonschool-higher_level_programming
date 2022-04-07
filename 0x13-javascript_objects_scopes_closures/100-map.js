af14fb087f4b5a5980be76a98b198d500716f53c
Hugo Machacon <hmachacom@gmail.com>
2022-04-07 05:13:10 -0500

    yessss

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const list = require('./100-main').list;
+const list = require('./100-data').list;
 
 console.log(list);
 
-const newList = list.map(function (x, idx){
-	return x * idx;
-})
+const newList = list.map(function (x, idx) {
+  return x * idx;
+});
 
 console.log(newList);
