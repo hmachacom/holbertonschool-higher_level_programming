486de4be361269a836e29eaa3fa200f5f4c8c66b
Hugo Machacon <hmachacom@gmail.com>
2022-05-10 10:49:05 -0500

    err

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], port=3306, db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
 
     for states in cur.fetchall():
         if states[1] == argv[4]:
