847c9d19bd42f949cf24a4c4c9c7822ce6fb21cf
Hugo Machacon <hmachacom@gmail.com>
2022-05-10 17:05:23 -0500

    9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    query = session.query(State).order_by(State.id).first()
+
+    for date in query:
+        print(f"{date.id}: {date.name}")
